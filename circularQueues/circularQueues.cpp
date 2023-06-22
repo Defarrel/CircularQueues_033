1cc26aeb06d12233dac8893feb9bceeca97429d9
defarrel <Defarrel>
2023-06-22 11:10:41 +0700

    set front_position menjadi 0

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -73,8 +73,15 @@ void insert() {
 			while (front_position <= max - 1) {
 				cout << queue_array[front_position] << "     ";
 				front_position++;
-			} 
-		
+			}
+			front_position = 0;
+
+			while (front_position <= rear_position) {
+				cout << queue_array[front_position] << "      ";
+				front_position++;
+
+			}
+			cout << endl;
 		}
 	}
 };
