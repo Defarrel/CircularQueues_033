b214664236ea73f857803e06147c63b666d35620
defarrel <Defarrel>
2023-06-22 10:55:19 +0700

    Mengecek jika ada satu element di antrian

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -40,6 +40,15 @@ void insert() {
 			return;
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[front] << "\n";
-	
+		if (front == rear) {
+			front = -1;
+			rear = -1;
+		}
+		else {
+		if (front == max - 1)
+			front = 0;
+		else
+			front = front + 1;
+		}
 	}
 };
